885851dc53dda32dd51d5917ad67cb1909b1512d
byuglya <byuglya91@gmail.com>
2025-11-25 23:55:10 +0800

    Исправленный тест

--- test_lab3.py
+++ test_lab3.py
@@ -10,7 +10,7 @@ from lesson_testing import (
 )
 
 @pytest.mark.parametrize("a, b, c, expected", [
-    (1, -3, 2, {0.0, 2.0}),
+    (1, -3, 2, {1.0, 2.0}),
     (1, 2, 1, {-1.0}),
     (1, 0, 1, set()),
 ])
